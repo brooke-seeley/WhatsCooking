6117945ff2708183521b45df7ad942155305b3b0
brooke-seeley <brookeseeleyaz@gmail.com>
2025-12-03 21:51:10 -0700

    Tried TD-IDF

---
+++ CookingAnalysis.R
@@ -0,0 +1,183 @@
+library(tidyverse)
+library(jsonlite)
+library(tidytext)
+library(tidymodels)
+library(vroom)
+library(textrecipes)
+
+## Initial Work
+#####
+# 
+# ### Read in Training Data
+# 
+# trainData <- read_file('train.json') %>%
+#   fromJSON() %>%
+#   mutate(cuisine = as.factor(cuisine))
+# 
+# ### Read in Test Data
+# 
+# testData <- read_file('test.json') %>%
+#   fromJSON()
+# 
+# ### Potential Features - Ingredient Count, Salt or Not, Baking
+# 
+# trainData <- trainData %>%
+#   mutate(salted = as.integer(map_lgl(ingredients, ~ "salt" %in% .x))) %>%
+#   mutate(baking = as.integer(map_lgl(ingredients, ~ 
+#                                        any(tolower(.x) %in% 
+#                                              c("baking soda", 
+#                                                "baking powder"))))) %>%
+#   mutate(count = map_int(ingredients, length)) %>%
+#   mutate(cuisine = as.factor(cuisine))
+# 
+# testData <- testData %>%
+#   mutate(salted = as.integer(map_lgl(ingredients, ~ "salt" %in% .x))) %>%
+#   mutate(baking = as.integer(map_lgl(ingredients, ~ 
+#                                        any(tolower(.x) %in% 
+#                                              c("baking soda", 
+#                                                "baking powder"))))) %>%
+#   mutate(count = map_int(ingredients, length))
+# 
+# cook_recipe <- recipe(cuisine ~ ., data = trainData) %>%
+#   step_rm(id, ingredients) %>%
+#   step_mutate(salted = factor(salted)) %>%
+#   step_mutate(baking = factor(baking))
+# 
+# cook_prep <- prep(cook_recipe)
+# bake(cook_prep, new_data = trainData)
+# 
+# ### Trying Random Forest
+# 
+# library(rpart)
+# 
+# tree_mod <- rand_forest(mtry=tune(),
+#                         min_n=tune(),
+#                         trees=100) %>%
+#   set_engine("ranger") %>%
+#   set_mode("classification")
+# 
+# tree_workflow <- workflow() %>%
+#   add_recipe(cook_recipe) %>%
+#   add_model(tree_mod)
+# 
+# #### Grid of values to tune Over
+# 
+# tuning_grid <- grid_regular(mtry(range=c(1,3)),
+#                             min_n(),
+#                             levels=5)
+# 
+# #### CV
+# 
+# folds <- vfold_cv(trainData, v = 5, repeats = 1)
+# 
+# CV_results <- tree_workflow %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=(metric_set(roc_auc)))
+# 
+# #### Find best tuning parameters
+# 
+# bestTune <- CV_results %>%
+#   select_best(metric="roc_auc")
+# 
+# #### Finalize workflow
+# 
+# final_wf <-
+#   tree_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=trainData)
+# 
+# #### Predict
+# 
+# tree_predictions <- final_wf %>%
+#   predict(new_data = testData, type="class")
+# 
+# #### Kaggle
+# 
+# tree_kaggle_submission <- tree_predictions %>%
+#   bind_cols(., testData) %>%
+#   select(id, .pred_class) %>%
+#   rename(cuisine=.pred_class)
+# 
+# vroom_write(x=tree_kaggle_submission, file="./RandForPreds.csv", delim=',')
+# 
+#####
+
+## Trying TF-IDF
+#####
+
+### Read in Data
+
+trainData <- read_file("train.json") %>%
+  fromJSON() %>%
+  step_mutate(cuisine = factor(cuisine))
+
+testData <- read_file("test.json") %>%
+  fromJSON()
+
+### Recipe
+
+tfidf_recipe <- recipe(cuisine ~ ingredients, data = trainData) %>%
+  step_mutate(ingredients = tokenlist(ingredients)) %>%
+  step_tokenfilter(ingredients, max_tokens=500) %>%
+  step_tfidf(ingredients)
+
+tfidf_prep <- prep(tfidf_recipe)
+bake(tfidf_prep, new_data = trainData)
+
+### Random Forest
+
+library(rpart)
+
+tree_mod <- rand_forest(mtry=tune(),
+                        min_n=tune(),
+                        trees=100) %>%
+  set_engine("ranger") %>%
+  set_mode("classification")
+
+tree_workflow <- workflow() %>%
+  add_recipe(tfidf_recipe) %>%
+  add_model(tree_mod)
+
+#### Grid of values to tune Over
+
+tuning_grid <- grid_regular(mtry(range = c(1,20)),
+                            min_n(),
+                            levels=5)
+
+#### CV
+
+folds <- vfold_cv(trainData, v = 5, repeats = 1)
+
+CV_results <- tree_workflow %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=(metric_set(accuracy)))
+
+#### Find best tuning parameters
+
+bestTune <- CV_results %>%
+  select_best(metric="accuracy")
+
+#### Finalize workflow
+
+final_wf <-
+  tree_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=trainData)
+
+#### Predict
+
+tree_predictions <- final_wf %>%
+  predict(new_data = testData, type="class")
+
+#### Kaggle
+
+tree_kaggle_submission <- tree_predictions %>%
+  bind_cols(., testData) %>%
+  select(id, .pred_class) %>%
+  rename(cuisine=.pred_class)
+
+vroom_write(x=tree_kaggle_submission, file="./RF_TDIDF_Preds.csv", delim=',')
+
+#####
